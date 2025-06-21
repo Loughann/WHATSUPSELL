b175fe9f40707e7b8472ab0f5c9afffd34afd152
Loughann <loughanpk2018@gmail.com>
2025-06-21 02:12:08 -0300

    first commit

--- app/upsell-page.tsx
+++ app/upsell-page.tsx
@@ -43,7 +43,7 @@ export default function UpsellPage() {
           </p>
           {/* CTA PRINCIPAL */}
           <a
-            href="https://pay.kirvano.com/a186b412-8355-4182-8011-56673bf28f5d"
+            href="https://checkoutwhats.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -94,7 +94,7 @@ export default function UpsellPage() {
             sua ÚNICA chance de ter o controle total e a paz que só a verdade pode trazer. Garanta agora!
           </p>
           <a
-            href="https://pay.kirvano.com/a186b412-8355-4182-8011-56673bf28f5d"
+            href="https://checkoutwhats.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
